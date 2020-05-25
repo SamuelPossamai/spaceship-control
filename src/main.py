a9e2fb23ca640fef7cacf1909e6077bd409dfa7b
Samuel Possamai <possamai.sam@gmail.com>
2020-05-25 14:33:08 -0300

    Can load scenario from command line

--- src/main.py
+++ src/main.py
@@ -1,16 +1,34 @@
 
 import sys
+import argparse
+from collections import namedtuple
 
 from PyQt5.QtWidgets import QApplication
 
 from .interface.mainwindow import MainWindow
 
+ProgramArgsInfo = namedtuple('ProgramArgsInfo', ('scenario',))
+
+def getProgramArguments():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--scenario', help='Scenario that will be loaded')
+
+    args = parser.parse_args()
+
+    return ProgramArgsInfo(scenario=args.scenario)
+
 def main():
+
+    program_args = getProgramArguments()
 
     app = QApplication(sys.argv)
 
     window = MainWindow()
     window.show()
+
+    if program_args.scenario is not None:
+        window.loadScenario(program_args.scenario)
 
     sys.exit(app.exec_())
 
