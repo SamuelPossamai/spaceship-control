60eb8c0e630c607157a9972b98a08f500a7534c7
Samuel Possamai <possamai.sam@gmail.com>
2020-08-03 19:38:57 -0300

    Updating example

--- examples/controllers/goals/firefive_goto.py
+++ examples/controllers/goals/firefive_goto.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import math
 
 from pathlib import Path
 
@@ -15,9 +16,13 @@ debug(ship.device)
 
 args_info = json.loads(sys.argv[1])
 
-goto_objectives = [objective_info.get('info') for objective_info in
+goto_objectives = (objective_info.get('info') for objective_info in
                    args_info.get('objectives', ())
-                   if objective_info.get('type') == 'GoToObjective']
+                   if objective_info.get('type') == 'GoToObjective')
+
+goto_objectives = [(objective_info.get('target-x'),
+                   objective_info.get('target-y'))
+                   for objective_info in goto_objectives]
 
 debug(goto_objectives)
 
@@ -28,8 +33,13 @@ for engine in engines:
 
 current_engine = 0
 
+objectives_iter = iter(goto_objectives)
+target = next(objectives_iter)
+
 while True:
     try:
+        debug(target)
+
         position = ship.position
         angle = ship.angle
         speed = ship.speed
@@ -40,6 +50,14 @@ while True:
         engines[current_engine].intensity = 4*random.random()
 
         current_engine = (current_engine + 1) % len(engines)
+
+        if math.isclose(position[0], target[0], rel_tol=0, abs_tol=10) and \
+            math.isclose(position[1], target[1], rel_tol=0, abs_tol=10):
+
+            target = next(objectives_iter)
+
+            if target is None:
+                sys.exit()
 
     except BrokenPipeError:
         break
