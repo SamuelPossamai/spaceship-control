b8ce504de683be65c751c0725eeb42fc04b7ae89
Samuel Possamai <possamai.sam@gmail.com>
2020-08-03 19:18:30 -0300

    Modifying objectives example library

--- examples/controllers/lib/goals.py
+++ examples/controllers/lib/goals.py
@@ -1,16 +1,18 @@
 
 class Goal:
 
-    def __init__(self, goals_info):
+    _GOAL_TYPE_MAP = {}
 
-        self.__goal_info = goals_info
-        self.__type = goals_info.get('type')
-        self.__name = goals_info.get('name')
-        self.__description = goals_info.get('description')
-        self.__info = goals_info.get('info')
+    def __init__(self, goal_info):
 
-    def load(goals_info):
-        return Goal(goals_info)
+        self.__goal_info = goal_info
+        self.__type = goal_info.get('type')
+        self.__name = goal_info.get('name')
+        self.__description = goal_info.get('description')
+        self.__info = goal_info.get('info')
+
+    def load(goal_info):
+        return Goal(goal_info)
 
     @property
     def type_(self):
@@ -27,3 +29,26 @@ class Goal:
     @property
     def info(self):
         return self.__info
+
+    def __repr__(self):
+        return f'Goal: {self.__name}'
+
+class GoToGoal(Goal):
+
+    def __init__(self, goal_info):
+        super().__init__(goal_info)
+
+        info = self.info
+
+        self.__pos = info.get('x-position'), info.get('y-position')
+        self.__distance = info.get('distance')
+
+    @property
+    def pos(self):
+        return self.__pos
+
+    @property
+    def distance(self):
+        return self.__distance
+
+Goal._GOAL_TYPE_MAP['GoToObjective'] = GoToGoal
