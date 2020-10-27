a284e8730c39ee2439caa1f7aa20d45f081807bd
Samuel Possamai <possamai.sam@gmail.com>
2020-10-27 20:36:24 -0300

    Now shape group elasticity and friction is propagated

--- src/storage/loaders/shapeloader.py
+++ src/storage/loaders/shapeloader.py
@@ -37,14 +37,20 @@ class ShapeLoader(CustomLoader):
     @staticmethod
     def __createCustomStaticShapeFunction(
             shape_content: 'MutableMapping[str, Any]', loader,
-            custom_shape_info: 'MutableMapping[str, Any]'):
+            custom_shape_info: 'MutableMapping[str, Any]',
+            default_elasticity: float = None,
+            default_friction: float = None):
 
-        return loader.load((shape_content,))
+        return loader.load((shape_content,),
+                           default_elasticity=default_elasticity,
+                           default_friction=default_friction)
 
     @staticmethod
     def __createCustomDynamicShapeFunction(
             shape_content: 'MutableMapping[str, Any]', loader,
-            custom_shape_info: 'MutableMapping[str, Any]'):
+            custom_shape_info: 'MutableMapping[str, Any]',
+            default_elasticity: float = None,
+            default_friction: float = None):
 
         variables = {variable['id']: variable['value'] for variable in
                      shape_content.get('Variable', ())}
@@ -53,15 +59,21 @@ class ShapeLoader(CustomLoader):
                                          variables=variables,
                                          enabled=True)
 
-        return loader.load((shape_content,))
+        return loader.load((shape_content,),
+                           default_elasticity=default_elasticity,
+                           default_friction=default_friction)
 
-    def load(self, info_list: 'Sequence[Dict[str, Any]]') \
+    def load(self, info_list: 'Sequence[Dict[str, Any]]',
+             default_elasticity: float = None,
+             default_friction: float = None) \
             -> 'Tuple[pymunk.Shape, ...]':
 
         shapes = []
 
         for shape_info in info_list:
-            new_shapes = self.__createShape(shape_info)
+            new_shapes = self.__createShape(
+                shape_info, default_elasticity=default_elasticity,
+                default_friction=default_friction)
             if new_shapes is not None:
                 if isinstance(new_shapes, Shape):
                     shapes.append(new_shapes)
@@ -70,7 +82,9 @@ class ShapeLoader(CustomLoader):
 
         return tuple(shapes)
 
-    def __createShape(self, info: 'Dict[str, Any]') -> 'pymunk.Shape':
+    def __createShape(self, info: 'Dict[str, Any]',
+                      default_elasticity: float = None,
+                      default_friction: float = None) -> 'pymunk.Shape':
 
         type_ = info.get('type')
 
@@ -79,36 +93,58 @@ class ShapeLoader(CustomLoader):
         if create_func is None:
             raise Exception(f'Invalid shape type \'{type_}\'')
 
-        return create_func(self, info)
+        return create_func(self, info, default_elasticity=default_elasticity,
+                           default_friction=default_friction)
 
     @staticmethod
     def __setGeneralProperties(shape: 'pymunk.Shape',
-                               info: 'Dict[str, Any]') -> None:
+                               info: 'Dict[str, Any]',
+                               default_elasticity: float = None,
+                               default_friction: float = None) -> None:
+
+        if default_elasticity is None:
+            default_elasticity = 0.5
+
+        if default_friction is None:
+            default_friction = 0.5
 
         shape.mass = info.get('mass', 0)
-        shape.elasticity = info.get('elasticity', 0.5)
-        shape.friction = info.get('friction', 0.5)
+        shape.elasticity = info.get('elasticity', default_elasticity)
+        shape.friction = info.get('friction', default_friction)
 
-    def __createCircleShape(self, info: 'Dict[str, Any]') -> 'pymunk.Shape':
+        print(shape.elasticity)
+
+    def __createCircleShape(self, info: 'Dict[str, Any]',
+                            default_elasticity: float = None,
+                            default_friction: float = None) -> 'pymunk.Shape':
 
         shape = Circle(None, info['radius'],
                        (info.get('x', 0), info.get('y', 0)))
 
-        self.__setGeneralProperties(shape, info)
+        self.__setGeneralProperties(shape, info,
+                                    default_elasticity=default_elasticity,
+                                    default_friction=default_friction)
 
         return shape
 
-    def __createPolyShape(self, info: 'Dict[str, Any]') -> 'pymunk.Shape':
+    def __createPolyShape(self, info: 'Dict[str, Any]',
+                          default_elasticity: float = None,
+                          default_friction: float = None) -> 'pymunk.Shape':
 
         points = tuple((point.get('x', 0), point.get('y', 0))
                        for point in info['Point'])
         shape = Poly(None, points)
 
-        self.__setGeneralProperties(shape, info)
+        self.__setGeneralProperties(shape, info,
+                                    default_elasticity=default_elasticity,
+                                    default_friction=default_friction)
 
         return shape
 
-    def __createRectangleShape(self, info: 'Dict[str, Any]') -> 'pymunk.Shape':
+    def __createRectangleShape(self, info: 'Dict[str, Any]',
+                               default_elasticity: float = None,
+                               default_friction: float = None) \
+                                   -> 'pymunk.Shape':
 
         pos_x = info.get('x')
         pos_y = info.get('y')
@@ -128,11 +164,15 @@ class ShapeLoader(CustomLoader):
             (pos_x, pos_y), (pos_x + width, pos_y),
             (pos_x + width, pos_y + height), (pos_x, pos_y + height)))
 
-        self.__setGeneralProperties(shape, info)
+        self.__setGeneralProperties(shape, info,
+                                    default_elasticity=default_elasticity,
+                                    default_friction=default_friction)
 
         return shape
 
-    def __createLineShape(self, info: 'Dict[str, Any]') -> 'pymunk.Shape':
+    def __createLineShape(self, info: 'Dict[str, Any]',
+                          default_elasticity: float = None,
+                          default_friction: float = None) -> 'pymunk.Shape':
 
         points = info.get('Point', ())
 
@@ -151,16 +191,28 @@ class ShapeLoader(CustomLoader):
 
         shape = Segment(None, points[0], points[1], info.get('thickness', 1))
 
-        self.__setGeneralProperties(shape, info)
+        self.__setGeneralProperties(shape, info,
+                                    default_elasticity=default_elasticity,
+                                    default_friction=default_friction)
 
         return shape
 
-    def __createShapeGroup(self, info: 'Dict[str, Any]') -> 'pymunk.Shape':
+    def __createShapeGroup(self, info: 'Dict[str, Any]',
+                          default_elasticity: float = None,
+                          default_friction: float = None) -> 'pymunk.Shape':
 
         shapes_info = info.get('Shape')
 
         if shapes_info:
-            return self.load(shapes_info)
+
+            default_elasticity = info.get(
+                'elasticity', default_elasticity)
+
+            default_friction = info.get('friction', default_friction)
+
+            return self.load(shapes_info,
+                             default_elasticity=default_elasticity,
+                             default_friction=default_friction)
 
         return None
 
